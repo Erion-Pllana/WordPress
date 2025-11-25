e096e0fe66f0c1c46ed129dc0c633bfdfb247375
Erion-Pllana <erionpllana35@gmail.com>
2025-11-25 17:34:03 +0100

    .

---
+++ function.php
@@ -0,0 +1,35 @@
+<?php
+function load_scripts(){
+    wp_enqueue_style('style', get_stylesheet_uri());
+
+
+    wp_enuqeue_script('dropdown', get_template_directory_uri(). '/js/dropdown.js', array(), '1.0', false);
+}
+add_action('wp_enqueue_scripts', 'load_scripts');
+
+function config(){
+    register_nav_menus(
+        array(
+            'wp_devs_main_menu' => 'mein Menu',
+            'wp_devs_footer_menu' => 'Footer Menu'
+        )
+        );
+
+    $args = array(
+        'height' => 225,
+        'width' => 1920
+    );
+    add_theme_support('custom-header', $args)
+    add_theme_support('post-thumbnail')
+    add_theme_support('custom-logo', $array(
+        'width' => 200,
+        'height' =>110,
+        'flex-height' => true,
+        'flex-width' => true
+    ));
+    add_theme_support('auomatic-feed-links');
+    add_theme_suppor('html5', array('commit-list', 'search-form', 'gallery', 'caption', 'style', 'cript'));
+    add_theme_support('title-tag');
+}
+add_action('after_setup_theme','config',0);
+?>
