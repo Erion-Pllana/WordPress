5961da6db50cff9a85b78228ee8a17aed828a0e0
Erion-Pllana <erionpllana35@gmail.com>
2025-11-27 17:40:13 +0100

    .

---
+++ parts/content-search.php
@@ -0,0 +1,8 @@
+<article id="post-<?php the_ID();?>" <?php post_class();?>>
+
+    <header>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_permalink(); ?></a></h2>
+        
+    </header>
+
+</article>
